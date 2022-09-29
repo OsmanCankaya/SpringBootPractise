9a332a00ec942c89baea11ebe938f83b18aa3eff
Osman Çankaya <osmancankayabilmuh@gmail.com>
2022-09-29 15:59:11 +0300

    Created database

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -5,13 +5,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+//spring
+@Entity
+@Table
+//lombok
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class Student {
+
+    //Sequence yapısı, mysql deki autoincrement yapısına karşılık geliyor
+    //allocationSize artış miktarını belirtiyoruz
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
