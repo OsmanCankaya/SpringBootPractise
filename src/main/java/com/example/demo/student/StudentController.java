6e05a92f559dad9c8a3599b35fb1ca29089d3211
Osman Çankaya <osmancankayabilmuh@gmail.com>
2022-09-29 14:38:27 +0300

    StudentService object created with dependency injection

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,12 +1,11 @@
 package com.example.demo.student;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
@@ -14,7 +13,8 @@ import java.util.List;
 @AllArgsConstructor
 public class StudentController {
 
-    private final StudentService studentService;;
+    @Autowired
+    private final StudentService studentService;
     @GetMapping
     public List<Student> getStudents(){
         return studentService.getStudents();
