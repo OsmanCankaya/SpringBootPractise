e33fa0f32081a72e8b241938695a59257773fc17
Osman Çankaya <osmancankayabilmuh@gmail.com>
2022-09-29 13:26:53 +0300

    Added Student Entity Added Lombok Library

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,27 @@
+package com.example.demo.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    //date of birth
+    private LocalDate dob;
+    private Integer age;
+
+    public Student(String name, String email, LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+    }
+}
