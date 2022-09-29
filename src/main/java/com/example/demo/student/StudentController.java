172d0d2b1649b44c1ac97159fd55a5447bcdbfec
Osman Çankaya <osmancankayabilmuh@gmail.com>
2022-09-29 13:47:01 +0300

    Added Student Service

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +11,12 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
+@AllArgsConstructor
 public class StudentController {
 
+    private final StudentService studentService;;
     @GetMapping
     public List<Student> getStudents(){
-        return List.of(new Student(
-                1L,
-                "Osman",
-                "osmancankaya@gmail.com",
-                LocalDate.of(2000, Month.JANUARY,1),
-                21));
+        return studentService.getStudents();
     }
 }
