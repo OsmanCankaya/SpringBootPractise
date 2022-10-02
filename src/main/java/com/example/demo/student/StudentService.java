904d1055abd7052056a523cbf999cd60d2ba07f9
Osman Çankaya <osmancankayabilmuh@gmail.com>
2022-10-02 03:30:48 +0300

    Created Srtudent update web service Fixed Error: Failed to configure a DataSource: 'url' attribute is not specified and no embedded datasource could be configured

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,10 +3,8 @@ package com.example.demo.student;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -34,23 +32,6 @@ public class StudentService {
             throw new IllegalStateException("Student with id " + id + " does not exist");
         }
         studentRepository.deleteById(id);
-    }
 
-    @Transactional
-    public void updateStudent(Long studentId, String name, String email) {
-        Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException((
-                "student with id " + studentId + " does not exist"
-        )));
-        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
-            student.setName(name);
-        }
-        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
-            Optional<Student> studentOptional = studentRepository
-                    .findStudentByIdEmail(email);
-            if (studentOptional.isPresent()) {
-                throw new IllegalStateException("email exist");
-            }
-            student.setEmail(email);
-        }
     }
 }
